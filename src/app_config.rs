d047cc0e7f078053ec0891222548589066541ed8
rzaharia <zahariarauul@gmail.com>
2024-03-26 23:37:31 +0200

    started working on download api function

--- src/app_config.rs
+++ src/app_config.rs
@@ -6,12 +6,13 @@ use crate::app_errors::AppErrors;
 pub static WEBHOOK_OBSERVED_REF: &str = "refs/heads/main";
 pub static WEBHOOK_COMMIT_TYPE_BOT: &str = "Bot";
 
-static EXPECTED_ENV_VARS: [&str; 5] = [
+static EXPECTED_ENV_VARS: [&str; 6] = [
     "CALLBACK_SECRET_TOKEN",
     "APP_NAME",
     "COMMIT_WHEN_SENDER_IS_BOT",
     "PRIVATE_KEY_FILE_LOC",
     "APP_ID",
+    "FILE_TO_DOWNLOAD",
 ];
 
 #[derive(Clone, Default)] //Clone needed by axum state
@@ -21,6 +22,7 @@ pub struct AppEnvVars {
     pub commit_when_sender_is_bot: bool,
     pub private_signature: String,
     pub app_id: u128,
+    pub file_to_download: String,
 }
 
 fn try_read_file(file_loc: &String) -> Result<String> {
@@ -59,7 +61,7 @@ impl AppEnvVars {
                             ));
                         };
                         result.private_signature = signature;
-                    },
+                    }
                     "APP_ID" => {
                         if let Ok(value) = value.parse::<u128>() {
                             result.app_id = value;
@@ -70,6 +72,7 @@ impl AppEnvVars {
                             ));
                         }
                     }
+                    "FILE_TO_DOWNLOAD" => result.file_to_download = value,
                     _ => {}
                 }
                 continue;
